c273e303eb92521d9553dd1c9b9da82d4cf6759b
Martin Høgh <mh@mapcentia.com>
2021-10-08 13:09:29 +0200

    Clean up

--- routes/index.php
+++ routes/index.php
@@ -1,14 +1,16 @@
 <?php
 /**
  * @author     Martin Høgh <mh@mapcentia.com>
- * @copyright  2013-2020 MapCentia ApS
+ * @copyright  2013-2021 MapCentia ApS
  * @license    http://www.gnu.org/licenses/#AGPL  GNU AFFERO GENERAL PUBLIC LICENSE 3
  *
  */
 
-use \app\inc\Route;
+use app\inc\Route;
+use app\models\Database;
+
 
 Route::add("extensions/geocoder/api/process/{db}",  function () {
     $db = Route::getParam("db");
-    \app\models\Database::setDb($db);
+    Database::setDb($db);
 });
